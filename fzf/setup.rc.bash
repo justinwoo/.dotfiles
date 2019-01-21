c4a4566b281060239540783783dca8ae015d7713
justinwoo <moomoowoo@gmail.com>
2019-01-21 23:01:33 +0200

    wtf i3 sucks

--- fzf/setup.rc.bash
+++ fzf/setup.rc.bash
@@ -1,5 +1,9 @@
 # shellcheck source=/dev/null
-source "$NIX_PROFILE"/share/fzf/key-bindings.bash
-source "$NIX_PROFILE"/share/fzf/completion.bash
+# Only load in interactive shells, not from a script or from scp
+if [ -n "$PS1" ]
+then
+  source "$NIX_PROFILE"/share/fzf/key-bindings.bash
+  source "$NIX_PROFILE"/share/fzf/completion.bash
+fi
 
 export FZF_DEFAULT_COMMAND='fd --type f'
