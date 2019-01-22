55aa36c3f38253337cc8b38fb65e9848b167c8cb
justinwoo <moomoowoo@gmail.com>
2019-01-22 15:56:04 +0200

    lsp

--- spacemacs/local-packages.nix
+++ spacemacs/local-packages.nix
@@ -20,6 +20,30 @@ in {
     sha256 = "191zj51fqfikqzf44f35vhx5d1dbp99m7kkhd75r3p3nmdvnn8i8";
   });
 
+  lsp-mode-local = mkLocalPackageDrv "lsp-mode-local"
+  (pkgs.fetchFromGitHub {
+    owner = "emacs-lsp";
+    repo = "lsp-mode";
+    rev = "6c13f29b8020d2327991eb0b7468deefaf108541";
+    sha256 = "05glsn4ianqj8i4m5naha21vrh6qf4v5r3g6l64kkq0v8qppz406";
+  });
+
+  lsp-ui-local = mkLocalPackageDrv "lsp-ui-local"
+  (pkgs.fetchFromGitHub {
+    owner = "emacs-lsp";
+    repo = "lsp-ui";
+    rev = "a8c50a93952afae3cd8948fecf4c1a3dc7f3e094";
+    sha256 = "1r4327fd8cvjxfwkddp5c4bdskyncbs4sx9m3z2w4d773y2jrakc";
+  });
+
+  company-lsp-local = mkLocalPackageDrv "company-lsp-local"
+  (pkgs.fetchFromGitHub {
+    owner = "tigersoldier";
+    repo = "company-lsp";
+    rev = "7167fa4547a83d6e07196ebf0b05c384b0a9a2c6";
+    sha256 = "1gxcj8mir4mdf4m4hh9napjaszcps00iyxq1rp01hnhq71iqzsms";
+  });
+
   nix-mode = mkLocalPackageDrv "nix-mode"
   (pkgs.fetchFromGitHub {
     owner = "nixos";
