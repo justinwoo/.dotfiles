6ae5048068943094da6f0da13f0ec3dc3f6a7f50
justinwoo <moomoowoo@gmail.com>
2019-02-10 22:27:50 +0200

    update easy-ps

--- nix/easy-ps.nix
+++ nix/easy-ps.nix
@@ -3,6 +3,6 @@
 import (pkgs.fetchFromGitHub {
   owner = "justinwoo";
   repo = "easy-purescript-nix";
-  rev = "54266e45aeaebc78dd51a40da36e9840a8a300dd";
-  sha256 = "1swjii6975cpys49w5rgnhw5x6ms2cc9fs8ijjpk04pz3zp2vpzn";
+  rev = "2bed26ac97f8a47f62c07ce3bc39ce05ca21c2b2";
+  sha256 = "1h7vkpwsfgynyv6q7xr6mzhg1v9j9j8y62hkskk0qq4d3b25wmz5";
 })
