b28c06d61d7b971975e8ac94f7034b17f3cd3593
justinwoo <moomoowoo@gmail.com>
2019-03-23 14:50:40 +0200

    update pinned

--- nix/pinned.nix
+++ nix/pinned.nix
@@ -1,6 +1,6 @@
 import (builtins.fetchTarball {
   name = "nixpkgs-justinwoo-fix-gitlab-fetch";
-  url = "https://github.com/justinwoo/nixpkgs/archive/b12c3bb050c42ca63a77907b2271446b74b59ca3.tar.gz";
+  url = "https://github.com/justinwoo/nixpkgs/archive/6e5ed122e4785ae1ead7a0df1184bc790ba0e0bc.tar.gz";
   sha256 = "01xhh5d0msn112zk3yvq6pr59asxxv4yqkyccxrpdb22wlcl9mhi";
 })
 
