dfaca6aa9d5393a81bb2f4580540f9730c989e33
justinwoo <moomoowoo@gmail.com>
2019-04-10 01:48:15 +0300

    update

--- nix/pinned.nix
+++ nix/pinned.nix
@@ -1,23 +1,7 @@
 import (builtins.fetchTarball {
-  name = "nixpkgs-justinwoo-fix-gitlab-fetch";
+  name = "nixpkgs-justinwoo";
 
-  url = "https://github.com/justinwoo/nixpkgs/archive/6e5ed122e4785ae1ead7a0df1184bc790ba0e0bc.tar.gz";
+  url = "https://github.com/justinwoo/nixpkgs/archive/11a6d01db6685344d1ef6d1363e2437be3a60614.tar.gz";
 
-  sha256 = "01xhh5d0msn112zk3yvq6pr59asxxv4yqkyccxrpdb22wlcl9mhi";
+  sha256 = "19nhqg569gna3n2x06phnsariy3fyb76az6w6m5nxd3x209qii9k";
 })
-
-# { pkgs ? import <nixpkgs> {} }:
-
-# import (pkgs.fetchFromGitHub {
-
-#   owner = "justinwoo";
-
-#   repo = "nixpkgs";
-
-#   # pinned to fix-gitlab-fetch...
-
-#   rev = "69d0465e50531b1c1ea81911f619acec1252c4d3";
-
-#   sha256 = "0ih046w3q8h1safl68lrgh97xyvscqip77lyrfamx34ps1azaani";
-
-# }) {}
