66d856640f35e3737b626f9059e7d5a315423cbd
justinwoo <moomoowoo@gmail.com>
2019-04-12 16:13:44 +0300

    update firefox

--- nix/firefox.nix
+++ nix/firefox.nix
@@ -4,9 +4,9 @@ pkgs.stdenv.mkDerivation {
   name = "firefox-simple";
 
   src = pkgs.fetchurl {
-    url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/65.0.1/linux-x86_64/en-US/firefox-65.0.1.tar.bz2";
+    url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/66.0.3/linux-x86_64/en-US/firefox-66.0.3.tar.bz2";
 
-    sha256 = "0mnb99w54bzak7wqb1l2c4hff6ki9ah3q8db2xg1nbc79ciax6m8";
+    sha256 = "04nrqhpwqh7i4h0h4llvwn3c8nnybclga785g3jk90pd4i7cqsps";
   };
 
   phases = "unpackPhase installPhase";
