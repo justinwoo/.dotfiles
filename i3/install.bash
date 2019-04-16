830e462ac193228ed1071c48c13206a0d712a723
justinwoo <moomoowoo@gmail.com>
2019-04-16 13:16:05 +0300

    dunst

--- i3/install.bash
+++ i3/install.bash
@@ -2,3 +2,10 @@ TARGET=~/.config/i3/config
 
 echo ensuring symlink for i3 config at "$TARGET"
 ln -sf "$DOT"/i3/config $TARGET
+
+echo ensuring dunstrc
+if [[ ! -e ~/.config/dunst/dunstrc ]]
+then
+  mkdir -p ~/.config/dunst
+  ln -s "$DOT/i3/dunstrc" ~/.config/dunst/dunstrc
+fi
