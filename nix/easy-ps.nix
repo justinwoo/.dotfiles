34921bde0b983905c05409254d648161231a813b
justinwoo <moomoowoo@gmail.com>
2019-05-03 05:27:30 -0700

    updates

--- nix/easy-ps.nix
+++ nix/easy-ps.nix
@@ -3,6 +3,6 @@
 import (pkgs.fetchFromGitHub {
   owner = "justinwoo";
   repo = "easy-purescript-nix";
-  rev = "180098aa9d2e64928a9c7727222a71b4d6658e1e";
-  sha256 = "0kf43ga3zc73vdv6kb8n8vqcxk9l31xpdhdlav23i0cmhkp137v6";
+  rev = "6f359bf0f11690886558f96c297305ede5a70650";
+  sha256 = "1ghh6gf6k7sxldrn9dsc5nwj0sk8sn89hyrz1m1w6qabzmkl75yd";
 })
