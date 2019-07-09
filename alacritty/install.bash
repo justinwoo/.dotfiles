3e88326b1768da305a175216d5e3e55d5e4e57a3
justinwoo <moomoowoo@gmail.com>
2019-07-09 14:07:29 +0300

    update alacritty install

--- alacritty/install.bash
+++ alacritty/install.bash
@@ -2,11 +2,5 @@
 
 TARGET=~/.config/alacritty
 
-if [[ ! -e $TARGET || $OVERRIDE == 'true' ]]
-then
-  echo Installing alacritty config to "$TARGET"
-  mkdir -p $TARGET
-  cp "$DOT/alacritty/alacritty.yml" $TARGET
-else
-  echo Skipping alacritty config installation.
-fi
+echo symlinking alacritty config to "$TARGET"
+ln -sf "$DOT/alacritty/alacritty.yml" $TARGET
