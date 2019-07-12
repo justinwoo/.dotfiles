383509460b46bd2e657dfea8f5e0de418cbafb0b
justinwoo <moomoowoo@gmail.com>
2019-07-12 10:57:11 +0300

    dhall bullshit

--- emacs.d.symlink/init.el
+++ emacs.d.symlink/init.el
@@ -563,6 +563,12 @@ If the error list is visible, hide it.  Otherwise, show it."
 (use-package dhall-mode
   :ensure t
   :mode "\\.dhall\\'")
+(setq dhall-format-arguments '("--ascii"))
+(setq dhall-format-at-save nil)
+
+(general-define-key
+ :keymaps 'normal
+ "SPC m p D" 'dhall-format-buffer)
 
 ;; haskell
 (use-package haskell-mode
