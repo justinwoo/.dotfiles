03ef1767079f875b896d5d1798a829e6ef472ba2
justinwoo <moomoowoo@gmail.com>
2019-07-23 14:26:12 +0300

    aweoifjaweofij

--- nix/pinned.nix
+++ nix/pinned.nix
@@ -1,5 +1,5 @@
 import (builtins.fetchTarball {
-  name = "nixpkgs-justinwoo";
-  url = "https://github.com/justinwoo/nixpkgs/archive/dc0dbaf0bd8ad9a04011ee734709b4f3e2ce15f0.tar.gz";
+  name = "nixpkgs-nixos";
+  url = "https://github.com/nixos/nixpkgs/archive/d54e52276bc381fd7093d44884fe4c6ad9629ad6.tar.gz";
   sha256 = "11yx4s18ydv1cpgr85q5146c1j0ncf2mqhjv4gqpyr7l5p73m5ky";
 })
