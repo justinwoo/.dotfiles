e0e1360833195fca6784cb65a75cbd03c07ee397
justinwoo <moomoowoo@gmail.com>
2019-08-10 14:13:55 +0300

    update lorrri

--- nix/lorri.nix
+++ nix/lorri.nix
@@ -3,8 +3,8 @@
 import (pkgs.fetchFromGitHub {
   owner = "justinwoo";
   repo = "my-lorri";
-  rev = "350a43902265a6b1bf7da5b057d07f6246b53555";
-  sha256 = "031rdy8ayarvfbh6qwqa0wppciiaa3zb12298b5768gzbyqvsjba";
+  rev = "579dbc6df776c0480d93a78435306a8fb191fb2b";
+  sha256 = "099zmcfgydbm8bg25c5cxqad9vshj2jc67w89qbqvy12zqilpv76";
 }) {
   inherit pkgs;
 }
