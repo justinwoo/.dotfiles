e62907225c1b7ca0709009b26d86ea001ffe07c8
justinwoo <moomoowoo@gmail.com>
2019-10-01 11:18:31 -0400

    might as well turn off menu bar again

--- emacs.d.symlink/init.el
+++ emacs.d.symlink/init.el
@@ -17,7 +17,7 @@
 (setq default-buffer-file-coding-system 'utf-8)
 (tool-bar-mode 0)
 (toggle-scroll-bar 0)
-(blink-cursor-mode 0)
+(menu-bar-mode -1)
 (setq default-fill-column 81)
 (setq fci-rule-column 81)
 (global-display-line-numbers-mode 1)
