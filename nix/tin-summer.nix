e37a1127c19d3c28b2118ba7b7641b96116f6060
justinwoo <moomoowoo@gmail.com>
2020-01-09 12:10:50 +0200

    update

--- nix/tin-summer.nix
+++ nix/tin-summer.nix
@@ -4,8 +4,8 @@ import (
   pkgs.fetchFromGitHub {
     owner = "justinwoo";
     repo = "tin-summer-derivation";
-    rev = "360f8d3b424a7fb2bc9267c4aee5fe7c9f2d4914";
-    sha256 = "0jaxghd2bsdidqb537lligl3wbcd6lxahkaq7b4jy8k2n1jdhvxi";
+    rev = "34c2d7539edf46096bb4b52639361674b143fdc3";
+    sha256 = "1w3aj0b7a17rrmjp8f9mbyjcx4llynqsm1r95m0vic8w29yqa3kl";
   }
 ) {
   inherit pkgs;
