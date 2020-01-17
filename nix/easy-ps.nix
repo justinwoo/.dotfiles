012fcf1aaf025b616a4de18d5b7ba76185744003
justinwoo <moomoowoo@gmail.com>
2020-01-17 09:53:15 +0200

    updates

--- nix/easy-ps.nix
+++ nix/easy-ps.nix
@@ -4,8 +4,8 @@ import (
   pkgs.fetchFromGitHub {
     owner = "justinwoo";
     repo = "easy-purescript-nix";
-    rev = "a09d4ff6a8e4a8a24b26f111c2a39d9ef7fed720";
-    sha256 = "1iaid67vf8frsqfnw1vm313d50mdws9qg4bavrhfhmgjhcyqmb52";
+    rev = "485471b86f764b9dd34e77ae767a629290f7f295";
+    sha256 = "0fh7sxpnp8krs091imksrs6w3vw31sq9kpz26mdw1kbpa8f0sgby";
   }
 ) {
   inherit pkgs;
