0d8a24464cbf501e1d3ec9cf4ab14ca0bc989e87
justinwoo <moomoowoo@gmail.com>
2020-01-31 10:45:51 +0200

    updates

--- nix/pinned.nix
+++ nix/pinned.nix
@@ -6,7 +6,7 @@ import (
   pkgs.fetchFromGitHub {
     owner = "nixos";
     repo = "nixpkgs";
-    rev = "cfc2ead8b3c2a5b60f0859e07a4646794bff52a0";
-    sha256 = "1gqg2fh7inlyr98v6yk7c2li6c60wfs7kn8yl663k4sz83b26jqx";
+    rev = "62bbc2abc1f3ae24943a204a4095c20737189656";
+    sha256 = "0cggas9zl19pzc0ikj9zdxvygqrgmj26snnmhhj27d25kh6cksww";
   }
 )
