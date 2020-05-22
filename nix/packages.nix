b23172bc99bb626c96413f3ade62b2818549f4d4
justinwoo <moomoowoo@gmail.com>
2020-05-22 14:35:13 +0300

    awoiefjaweifj

--- nix/packages.nix
+++ nix/packages.nix
@@ -25,11 +25,27 @@ let
       "z"
     ]
   );
+
+  dynamic-linker = pkgs.stdenv.cc.bintools.dynamicLinker;
+  buildInputs = [ pkgs.gmp pkgs.zlib pkgs.ncurses5 pkgs.stdenv.cc.cc.lib ];
+  libPath = pkgs.lib.makeLibraryPath buildInputs;
+
+  purs-0_13_7 = pkgs.runCommand "purs-local" rec {
+    src = pkgs.fetchurl {
+      url = "https://github.com/justinwoo/purescript/releases/download/v0.13.7/purs";
+      sha256 = "1dahif6z40acjsjm0clrq5hqhibdva6s3g8v49k9fjrvnfiwam77";
+    };
+  } ''
+    mkdir -p $out/bin
+    cp $src $out/bin/purs
+    chmod +xw $out/bin/purs
+    patchelf --interpreter ${dynamic-linker} --set-rpath ${libPath} $out/bin/purs
+  '';
 in
 my-pkgs // {
   inherit (pkgs) feh i3 i3status rofi scrot;
 
-  inherit (easy-ps.inputs) purs psc-package-simple;
+  inherit (easy-ps.inputs) psc-package-simple;
 
   inherit (easy-dhall) dhall-simple dhall-json-simple;
 
@@ -71,4 +87,6 @@ my-pkgs // {
     watchexec
     xdotool
     ;
+
+  inherit purs-0_13_7;
 }
