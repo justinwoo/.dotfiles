416da188a2259666973eab30ba2271292875f90c
justinwoo <moomoowoo@gmail.com>
2021-04-09 12:29:59 +0300

    aweoifjaoweifjawef

--- nix/nix-direnv.nix
+++ nix/nix-direnv.nix
@@ -4,7 +4,7 @@ import (
   pkgs.fetchFromGitHub {
     owner = "nix-community";
     repo = "nix-direnv";
-    rev = "52996098653a97b2ed2dbcccb191e9ed38505a47";
-    sha256 = "0g4rc772rmfmm39w8z9y7s65xp1wba9yglg2nhj187dqn2nxygfb";
+    rev = "a512304cdebbb655817cd4eac8b0e710a781a14f";
+    sha256 = "0537ak4316vg9h940shb9lyzchn9v52f0j2ak8pq0jm6glnha5j9";
   }
 ) {}
