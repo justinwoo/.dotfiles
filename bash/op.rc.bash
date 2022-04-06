696bdafa50e9b0f22a5badf1acefebf3c617eb6f
justinwoo <moomoowoo@gmail.com>
2022-04-06 17:39:06 +0300

    owjafoaiewjf

--- bash/op.rc.bash
+++ bash/op.rc.bash
@@ -11,16 +11,4 @@ op() {
   fi
 }
 
-_op() {
-  local cur opts
-  COMPREPLY=()
-  if [ "$COMP_CWORD" == 1 ]
-  then
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    opts="$(ls)"
-
-    # shellcheck disable=2207
-    COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
-  fi
-}
-complete -F _op op
+complete -o default -o bashdefault op
---
+++ gnome/install.bash
@@ -0,0 +1,3 @@
+# set xdg defaults
+xdg-mime default /home/justin/.nix-profile/share/applications/org.gnome.Evince.desktop application/pdf
+xdg-mime default emacs.desktop text/plain
