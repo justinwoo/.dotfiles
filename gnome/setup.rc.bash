426fe3d41b795ceec3517fcbfac0ea24a0e47ea2
justinwoo <moomoowoo@gmail.com>
2022-11-22 16:57:02 +0200

    owaiejfaiowejf

--- gnome/setup.rc.bash
+++ gnome/setup.rc.bash
@@ -1,6 +1,6 @@
 alias settings="XDG_CURRENT_DESKTOP=Unity gnome-control-center"
 
-if [[ -f $(command -v xclip) ]]
-then
-gsettings set org.gnome.desktop.sound event-sounds false
-fi
+# if [[ -f $(command -v gsettings) ]]
+# then
+# gsettings set org.gnome.desktop.sound event-sounds false
+# fi
