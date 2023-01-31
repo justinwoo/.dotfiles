ebd0a3d9aa250a3b950a8437ce674e0cefe6fefd
justinwoo <moomoowoo@gmail.com>
2023-01-31 11:41:24 -0600

    update

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -7,6 +7,8 @@ function toggleApp(appName)
         else
             app:activate()
         end
+    else
+        hs.alert.show("Could not find app from name: " .. appName)
     end
 end
 
@@ -19,11 +21,13 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function() hs.reload() end)
 hs.hotkey.bind({"alt"}, "`",
                function() hs.application.frontmostApplication():hide() end)
 
+bindApp({"alt", "shift"}, "p", "Finder")
 bindApp({"alt"}, "e", "emacs")
 bindApp({"alt"}, "o", "iTerm2")
 bindApp({"alt"}, "u", "Telegram")
 bindApp({"alt"}, "y", "Google Chrome")
 bindApp({"alt"}, "i", "Slack")
 bindApp({"alt"}, ";", "mpv")
+bindApp({"alt", "shift"}, ";", "FileZilla")
 
 hs.alert.show("Config loaded")
