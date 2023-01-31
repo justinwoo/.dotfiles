2581b66c27bbc837e4a62644102307e498adc140
justinwoo <moomoowoo@gmail.com>
2023-01-31 09:55:52 -0600

    more hammer

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -10,7 +10,20 @@ function toggleApp(appName)
     end
 end
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function() hs.reload() end)
-hs.alert.show("Config loaded")
+function bindApp(modifiers, key, appName)
+    hs.hotkey.bind(modifiers, key, function() toggleApp(appName) end)
+end
 
-hs.hotkey.bind({"alt"}, "R", function() toggleApp("emacs") end)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function() hs.reload() end)
+
+hs.hotkey.bind({"alt"}, "`",
+               function() hs.application.frontmostApplication():hide() end)
+
+bindApp({"alt"}, "e", "emacs")
+bindApp({"alt"}, "o", "iTerm2")
+bindApp({"alt"}, "u", "Telegram")
+bindApp({"alt"}, "y", "Google Chrome")
+bindApp({"alt"}, "i", "Slack")
+bindApp({"alt"}, ";", "mpv")
+
+hs.alert.show("Config loaded")
