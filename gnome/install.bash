13ff4c138db4478036c9e720edbff13bc7baf3d5
justinwoo <moomoowoo@gmail.com>
2023-07-14 20:27:25 +0300

    tweak installs

--- gnome/install.bash
+++ gnome/install.bash
@@ -1,3 +1,5 @@
 # set xdg defaults
+[[ $MACOS ]] && exit 0
+
 xdg-mime default /home/justin/.nix-profile/share/applications/org.gnome.Evince.desktop application/pdf
 xdg-mime default emacs.desktop text/plain
--- k9s/install.bash
+++ k9s/install.bash
@@ -1,3 +1,4 @@
 TARGET=~/.config/k9s/skin.yml
 
+mkdir -p "$(dirname "$TARGET")"
 ln -sf "$DOT"/k9s/skin.yml $TARGET
