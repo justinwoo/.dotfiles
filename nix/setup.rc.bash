3b851ddd6d756e6a1ef48eeaa93e243bbecf3662
justinwoo <moomoowoo@gmail.com>
2024-06-24 15:15:47 +0300

    oiwaejfaoeiwfj

--- nix/setup.rc.bash
+++ nix/setup.rc.bash
@@ -2,6 +2,7 @@ if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ];
     then "." "$HOME/.nix-profile/etc/profile.d/nix.sh";
 fi
 
+export NIXPKGS_ALLOW_UNFREE=1
 export SHELL_NIX="$HOME/.dotfiles/nix/shell.nix"
 export BASH_COMPLETION_USER_DIR="$HOME/.nix-profile/share/bash-completion/completions"
 export BAT_THEME='Monokai Extended Light'
