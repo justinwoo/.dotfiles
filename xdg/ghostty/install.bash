77ddf3d56dec18de551dc030dde6c9b1be9e753a
justinwoo <moomoowoo@gmail.com>
2025-01-04 17:18:16 +0200

    typo

--- xdg/ghostty/install.bash
+++ xdg/ghostty/install.bash
@@ -1,2 +1,2 @@
 echo ensuring no lingering ghostty config
-rm "$HOME/Library/Application\ Support/com.mitchellh.ghostty/config." || exit 0
+rm "$HOME/Library/Application\ Support/com.mitchellh.ghostty/config" || exit 0
