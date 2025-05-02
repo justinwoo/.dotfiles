c6cb29bd71b67576c58dd9b9fb1cccc358ca148c
justinwoo <moomoowoo@gmail.com>
2025-05-02 13:34:40 +0300

    update git prompt

--- bash/prompt.rc.bash
+++ bash/prompt.rc.bash
@@ -26,62 +26,41 @@ function my_prompt {
     local EXIT=$?
     local git_info=''
     local git_color=$GREEN
-    if git rev-parse --git-dir &> /dev/null; then
-        git_info=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
 
-        # shellcheck disable=2181
-        if ! git rev-parse --abbrev-ref HEAD &> /dev/null; then
+    if git rev-parse --is-inside-work-tree &> /dev/null; then
+        local git_status
+        git_status=$(git status --porcelain=v2 --branch 2>/dev/null)
+
+        git_info=$(echo "$git_status" | grep '^# branch.head' | awk '{print $3}')
+        if [[ -z $git_info ]]; then
             git_info="新"
             git_color=$RED
         fi
 
-        # dirty
-        if ! git diff --no-ext-diff --cached --quiet; then
+        if echo "$git_status" | grep -q '^1'; then
             git_info+="+"
             git_color=$YELLOW
         fi
-        if ! git diff --no-ext-diff --quiet; then
-            git_info+="*"
-            git_color=$RED
-        fi
 
-        # untracked
-        if [ "$(git config --bool bash.showUntrackedFiles)" != "false" ] &&
-               git ls-files --others --exclude-standard --directory --no-empty-directory --error-unmatch -- ':/*' \
-                   >/dev/null 2>/dev/null; then
+        if echo "$git_status" | grep -q '^?'; then
             git_info+=" 汚"
             git_color=$RED
         fi
 
-        # stash has trash
         if git rev-parse --verify --quiet refs/stash >/dev/null; then
             git_info+=" 庫"
             git_color=$YELLOW
         fi
 
-        # https://github.com/nojhan/liquidprompt/blob/eda83efe4e0044f880370ed5e92aa7e3fdbef971/liquidprompt#L864-L876
-        local gitdir
-        gitdir="$(\git rev-parse --git-dir 2>/dev/null)"
-        if [[ -f "${gitdir}/MERGE_HEAD" ]]; then
-            git_info+=" MERGING"
-            git_color=$RED
-        elif [[ -d "${gitdir}/rebase-apply" || -d "${gitdir}/rebase-merge" ]]; then
-            git_info+=" REBASING"
-            git_color=$RED
-        elif [[ -f "${gitdir}/CHERRY_PICK_HEAD" ]]; then
-            git_info+=" CHERRY-PICKING"
-            git_color=$RED
-        fi
-
         # shellcheck disable=2154
-        if [[ -z $MACOS ]]
-        then
+        local diff
+        if [[ -z $MACOS ]]; then
             __git_ps1_show_upstream
-            diff=$p
+            diff=""
         else
             GIT_PS1=$(__git_ps1)
             if [[ $GIT_PS1 == *"|"* ]]; then
-                diff=" $(__git_ps1  | cut -d '(' -f 2 | cut -d '|' -f 2 | cut -d ')' -f 1)"
+                diff=" $(__git_ps1 | cut -d '(' -f 2 | cut -d '|' -f 2 | cut -d ')' -f 1)"
             else
                 diff=""
             fi
@@ -92,10 +71,9 @@ function my_prompt {
 
         git_info+=' '
     fi
-    local prefix
 
-    if [ -z "$IN_NIX_SHELL"  ]
-    then
+    local prefix
+    if [ -z "$IN_NIX_SHELL" ]; then
         prefix="$NORMAL"馬
     else
         prefix="$BLUE"肉
@@ -103,15 +81,13 @@ function my_prompt {
 
     local jobs_info
     local jobs_count
-    jobs_count=$(($(jobs -r | wc -l)+$(jobs -s | wc -l)))
-    if [[ $jobs_count -ne 0 ]];
-    then
+    jobs_count=$(($(jobs -r | wc -l) + $(jobs -s | wc -l)))
+    if [[ $jobs_count -ne 0 ]]; then
         jobs_info="$jobs_count "
     fi
 
     local exit
-    if [[ $EXIT -ne 0 ]]
-    then
+    if [[ $EXIT -ne 0 ]]; then
         exit="$RED($EXIT) "
     fi
 
