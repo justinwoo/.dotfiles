95c0281f0ca79433bfd5ac589c53166df718ec69
justinwoo <moomoowoo@gmail.com>
2026-03-06 23:06:28 +0200

    new nixpkgs

--- nix/env.nix
+++ nix/env.nix
@@ -2,42 +2,39 @@
 let
   z = import ./z.nix { inherit pkgs; };
 
-  packages = (
-    with (pkgs.gitAndTools);
+  packages = (with pkgs;
     [
       git
       git-extras
       hub
-    ]
-  ) ++ (with pkgs; [
-    bash-completion
-    bat
-    colordiff
-    coreutils
-    direnv
-    fd
-    fzf
-    gh
-    gnutar
-    gnused
-    jq
-    neovim
-    nix-bash-completions
-    nixpkgs-fmt
-    nodejs_22
-    ripgrep
-    shellcheck
-    shfmt
-    sqlite-interactive
-    tig
-    tmux
-    vivid
-    watchexec
-    mermaid-cli
-    lazydocker
-  ] ++ [
-    z
-  ]);
+      bash-completion
+      bat
+      colordiff
+      coreutils
+      direnv
+      fd
+      fzf
+      gh
+      gnutar
+      gnused
+      jq
+      neovim
+      nix-bash-completions
+      nixpkgs-fmt
+      nodejs_22
+      ripgrep
+      shellcheck
+      shfmt
+      sqlite-interactive
+      tig
+      tmux
+      vivid
+      watchexec
+      mermaid-cli
+      lazydocker
+    ] ++ [
+      z
+    ]);
 
 in
 pkgs.buildEnv {
--- nix/pinned.nix
+++ nix/pinned.nix
@@ -1,6 +1,6 @@
 import (
   builtins.fetchTarball rec {
-    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05-pre.tar.gz";
-    sha256 = "0b96wqvk3hs98dhfrmdhqmx9ibac4kjpanpd1pig19jaglanqnxr";
+    url = "https://github.com/NixOS/nixpkgs/archive/917fec990948658ef1ccd07cef2a1ef060786846.tar.gz";
+    sha256 = "1x3hmj6vbza01cl5yf9d0plnmipw3ap6y0k5rl9bl11fw7gydvva";
   }
 )
